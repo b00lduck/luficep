e9cf1e523c1767f5ab9fde97ed7dc4a9e1c0b1e0
Daniel Zerlett <daniel@zerlett.eu>
2020-07-17 14:20:08 +0200

    more logging

--- src/handler/mqtt.rs
+++ src/handler/mqtt.rs
@@ -1,5 +1,28 @@
+#[cfg(test)]
+use mockall::{automock, predicate::*};
+
 use log::{info};
 
-pub fn handle_mqtt_message(msg: paho_mqtt::message::Message) {
-    info!("{}", msg);
+pub struct MqttHandler {}
+
+impl MqttHandler {}
+
+#[cfg_attr(test, automock)]
+pub trait HandleMqttMessage {
+    fn handle_mqtt_message(&self, msg: paho_mqtt::message::Message);
+}
+
+impl HandleMqttMessage for MqttHandler {
+    fn handle_mqtt_message(&self, msg: paho_mqtt::message::Message) {
+        info!("{}", msg);
+    }    
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() -> Result<(), String> {
+        assert_eq!(2 + 2, 4);
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod handler;
 
-pub use crate::handler::mqtt as mqtt_handler; 
+use crate::handler::mqtt as mqtt_handler;
+
+use crate::handler::mqtt::HandleMqttMessage;
 
 use std::{
     env,
@@ -16,7 +18,7 @@ use log::{info, warn, error};
 
 // The topics to which we subscribe.
 const TOPICS: &[&str] = &[ "test", "hello" ];
-const QOS: &[i32] = &[1, 1];
+const QOS: &[i32] = &[2, 2];
 
 fn main() {
     // Initialize the logger from the environment
@@ -30,7 +32,7 @@ fn main() {
     // A real system should try harder to use a unique ID.
     let create_opts = mqtt::CreateOptionsBuilder::new()
         .server_uri(host)
-        .client_id("rust_async_subscribe")
+        .client_id("lucifep")
         .finalize();
 
     // Create the client connection
@@ -39,42 +41,52 @@ fn main() {
         process::exit(1);
     });
 
+    let mqtt_handler = mqtt_handler::MqttHandler {};
+
     if let Err(err) = block_on(async {
         // Get message stream before connecting.
         let mut strm = cli.get_stream(25);
 
         // Define the set of options for the connection
-        let lwt = mqtt::Message::new("test", "Async subscriber lost connection",
-                                     mqtt::QOS_1);
+        let lwt = mqtt::Message::new("test", "Lucifep lost connection", mqtt::QOS_2);
 
         let conn_opts = mqtt::ConnectOptionsBuilder::new()
             .keep_alive_interval(Duration::from_secs(20))
             .mqtt_version(mqtt::MQTT_VERSION_3_1_1)
             .clean_session(false)
+            .keep_alive_interval(Duration::from_secs(15))
             .will_message(lwt)
             .finalize();
 
         // Make the connection to the broker
         info!("Connecting to the MQTT server...");
         cli.connect(conn_opts).await?;
+        info!("Connected to the MQTT server.");
 
         info!("Subscribing to topics: {:?}", TOPICS);
         cli.subscribe_many(TOPICS, QOS).await?;
 
         // Just loop on incoming messages.
-        info!("Waiting for messages...");
+        info!("Waiting for MQTT messages...");
 
         while let Some(msg_opt) = strm.next().await {            
             if let Some(msg) = msg_opt {
-                mqtt_handler::handle_mqtt_message(msg);                
-            }
-            else {
+                // A "Some" means we have a new message
+                mqtt_handler.handle_mqtt_message(msg)
+            } else {
                 // A "None" means we were disconnected. Try to reconnect...
                 warn!("Lost connection. Attempting reconnect.");
-                while let Err(err) = cli.reconnect().await {
-                    error!("Error reconnecting: {}", err);
-                    // For tokio use: tokio::time::delay_for()
-                    async_std::task::sleep(Duration::from_millis(1000)).await;
+                loop {
+                    match cli.reconnect().await {
+                        Err(err) => {
+                            error!("Error reconnecting: {}", err);                    
+                            async_std::task::sleep(Duration::from_millis(2000)).await;
+                        }
+                        Ok(_) => {
+                            info!("Reconnected to the MQTT server.");
+                            break;
+                        }
+                    }
                 }
             }
         }
@@ -83,13 +95,5 @@ fn main() {
         Ok::<(), mqtt::Error>(())
     }) {
         error!("{}", err);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
     }
 }
